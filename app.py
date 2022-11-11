4dc7a9e741376d920f8f369619ffba751b7ec932
widm2240 <widm@naver.com>
2022-11-11 17:07:14 +0900

    updated

--- app.py
+++ app.py
@@ -52,3 +52,46 @@ def showHello():
     }
 
     return responseBody
+
+## 메인 로직!! 
+def cals(opt_operator, number01, number02):
+    if opt_operator == "addition":
+        return number01 + number02
+    elif opt_operator == "subtraction": 
+        return number01 - number02
+    elif opt_operator == "multiplication":
+        return number01 * number02
+    elif opt_operator == "division":
+        return number01 / number02
+
+import json
+
+## 카카오톡 Calculator 계산기 응답
+@app.route('/api/calCulator', methods=['POST'])
+def calCulator():
+    body = request.get_json()
+    print(body)
+    params_df = body['action']['params']
+    print(type(params_df))
+    opt_operator = params_df['operators']
+    number01 = json.loads(params_df['sys_number01'])['amount']
+    number02 = json.loads(params_df['sys_number02'])['amount']
+
+    print(opt_operator, type(opt_operator), number01, type(number01))
+
+    answer_text = str(cals(opt_operator, number01, number02))
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
