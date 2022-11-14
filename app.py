f3c44c6fad863c8a5f93a334fd2d57aec94efc34
widm2240 <widm@naver.com>
2022-11-14 14:23:00 +0900

    updated

--- app.py
+++ app.py
@@ -74,6 +74,7 @@ def calCulator():
     params_df = body['action']['params']
     print(type(params_df))
 
+    # print를 통해 디버깅 하는 방법
     print('----')
     opt_operator = params_df['operators']
     print('operator:', opt_operator)
@@ -100,3 +101,171 @@ def calCulator():
     }
 
     return responseBody
+
+# -*- coding: utf-8 -*-
+from flask import Flask, request
+import pandas as pd 
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.sql import text 
+import json
+
+## DB 연결 Local
+def db_create():
+    # 로컬
+	# engine = create_engine("postgresql://postgres:1234@localhost:5432/chatbot", echo = False)
+		
+	# Heroku
+    engine = create_engine("postgres://upkewotgwcdzpq:1d2c63a3d6f5af6b5293420ceab85d12f396b5898126de19ce0a5337147e4df0@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d6njb4ouuue8kd", echo = False)
+
+    engine.connect()
+    engine.execute("""
+        CREATE TABLE IF NOT EXISTS iris(
+            sepal_length FLOAT NOT NULL,
+            sepal_width FLOAT NOT NULL,
+            pepal_length FLOAT NOT NULL,
+            pepal_width FLOAT NOT NULL,
+            species VARCHAR(100) NOT NULL
+        );"""
+    )
+    data = pd.read_csv('data/iris.csv')
+    print(data)
+    data.to_sql(name='iris', con=engine, schema = 'public', if_exists='replace', index=False)
+
+## 메인 로직!! 
+def cals(opt_operator, number01, number02):
+    if opt_operator == "addition":
+        return number01 + number02
+    elif opt_operator == "subtraction": 
+        return number01 - number02
+    elif opt_operator == "multiplication":
+        return number01 * number02
+    elif opt_operator == "division":
+        return number01 / number02
+
+application = Flask(__name__)
+
+@application.route("/")
+def index():
+    db_create()
+    return "DB Created Done !!!!!!!!!!!!!!!"
+
+## 카카오톡 텍스트형 응답
+@application.route('/api/sayHello', methods=['POST'])
+def sayHello():
+    body = request.get_json()
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "안녕 hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+
+## 카카오톡 이미지형 응답
+@application.route('/api/showHello', methods=['POST'])
+def showHello():
+    body = request.get_json()
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleImage": {
+                        "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+                        "altText": "hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+## 카카오톡 Calculator 계산기 응답
+@application.route('/api/calCulator', methods=['POST'])
+def calCulator():
+    body = request.get_json()
+    print(body)
+    params_df = body['action']['params']
+    print(type(params_df))
+
+    print('-----')
+    opt_operator = params_df['operators']
+    print('operator:', opt_operator)
+    print('-----')
+    number01 = json.loads(params_df['sys_number01'])['amount']
+    number02 = json.loads(params_df['sys_number02'])['amount']
+
+    print(opt_operator, type(opt_operator), number01, type(number01))
+
+    answer_text = str(cals(opt_operator, number01, number02))
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+## Query 조회
+@application.route('/api/querySQL', methods=['POST'])
+def querySQL():
+    
+    body = request.get_json()
+    params_df = body['action']['params']
+    sepal_length_num = str(json.loads(params_df['sepal_length_num'])['amount'])
+
+    print(sepal_length_num, type(sepal_length_num))
+    query_str = f'''
+        SELECT sepal_length, species FROM iris where sepal_length >= {sepal_length_num}
+    '''
+
+    engine = create_engine("postgres://upkewotgwcdzpq:1d2c63a3d6f5af6b5293420ceab85d12f396b5898126de19ce0a5337147e4df0@ec2-18-215-41-121.compute-1.amazonaws.com:5432/d6njb4ouuue8kd", echo = False)
+
+    with engine.connect() as conn:
+        query = conn.execute(text(query_str))
+
+    df = pd.DataFrame(query.fetchall())
+    nrow_num = str(len(df.index))
+    answer_text = nrow_num
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text + "개 입니다."
+                    }
+                }
+            ]
+        }
+    }
+    return responseBody
+
+
+if __name__ == "__main__":
+    db_create()
+    application.run()
