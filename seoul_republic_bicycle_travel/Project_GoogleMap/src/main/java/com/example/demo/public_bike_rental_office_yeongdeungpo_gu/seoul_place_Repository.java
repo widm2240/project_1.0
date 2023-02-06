e58599a96417063979b47b65e839564d9012582e
widm2240 <widm@naver.com>
2023-02-06 10:51:35 +0900

    last upload

--- seoul_republic_bicycle_travel/Project_GoogleMap/src/main/java/com/example/demo/public_bike_rental_office_yeongdeungpo_gu/seoul_place_Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.public_bike_rental_office_yeongdeungpo_gu;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface seoul_place_Repository extends JpaRepository<seoul_recommend_course_yeongdeungpo_gu, String>{
-    
-}
--- seoul_republic_bicycle_travel/Project_NaverMap/bicycle_naver_version/src/main/java/com/example/demo/public_bike_rental_office_yeongdeungpo_gu/seoulService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.public_bike_rental_office_yeongdeungpo_gu;
-
-import java.util.List;
-
-public interface seoulService {
-    List<seoul_recommend_course_yeongdeungpo_gu> getAllplaces();
-}
